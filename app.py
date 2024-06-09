b519a5ef20db5e548444f1b664df14f168704a3f
Philomath2020 <suhrobjonibodullaev@gmail.com>
2024-06-09 07:26:29 +0500

    Files uploded

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from fastai.vision.all import *
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.PureWindowsPath
+
+#titLe
+st.title('Mevalarni klassifikatsiya qiluvchi model')
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'gif', 'svg', 'jpg'])
+
+if file:
+    st.image(file)
+
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner('fruits.pkl')
+
+    #prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f'Bashorat: {pred}')
+    st.info(f'Ehtimollik: {probs[pred_id]*100:.1f}%')
